bc4032f6f1fff53d5dc5ecaf48411960d08bf552
lagourou <agourou84@gmail.com>
2025-09-03 10:39:06 +0200

    Modification du contrôleur de /api/status

--- Project-Apply/src/main/java/com/projetApply/Project_Apply/controller/StatusController.java
+++ Project-Apply/src/main/java/com/projetApply/Project_Apply/controller/StatusController.java
@@ -3,14 +3,14 @@ package com.projetApply.Project_Apply.controller;
 import java.util.Map;
 
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
 @Profile("dev")
+@RestController
 @RequestMapping("/api")
 @Slf4j
 public class StatusController {
